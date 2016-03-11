91505d6c1a57f3634e8a6ac68183ffec1809bf9e
DREW <drewgamesaw@gmail.com>
2016-03-11 09:37:20 -0500

    Added bool for if ethans mod is installed
    TODO: Make this auto detect, and make this a config

--- src/main/java/com/drew/morefood/create/CreateItems.java
+++ src/main/java/com/drew/morefood/create/CreateItems.java
@@ -1,5 +1,13 @@
 package com.drew.morefood.create;
 
+/*/
+Drew's Minecraft Mods: More Food
+Purpose: Adds more food items for random blocks that are already in the game, like piston and stuff
+I know its weird but thats the point, supposed ot be stupid
+Created by Drew
+Dependencies: (OPTIONAL) Ethan's MSM
+/*/
+
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
@@ -13,6 +21,15 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class CreateItems 
 {
 	
+    /*/
+    TODO: Make this automatically detect if MSM is installed, this will toggle item recipies for some items
+    to use items from his mod, if the mod isnt installed, use a different recipe, 
+    for example: DiamondSwordSwordSteak, if mod installed: diamodnswordsword + steak,
+    if mod not installed: Just use ethan's recipe (diamondsword + diamondsword + stick) + steak
+    /*/
+    public static boolean isMsmInstalled = true;
+
+	
     // items:
     public static Item DiamondSteak;
     public static Item PistonSteak;
@@ -23,9 +40,18 @@ public class CreateItems
     public static Item TNTSteak; // DANGER!!!!
     public static Item DiamondSwordSwordSteak; // NEEDS ETHANS MOD FOR MSM:DIAMONDSWORDSWORD TO CRAFT
 	
+	
     public static void createItems() 
     {
     	
     }
     
+    public static void createRecipes()
+    {
+    	if(isMsmInstalled)
+    	{				  // True means we want msm recipes, false means we dont
+            foodRecipes.createFoodRecipes(true)
+    	}
+    	foodRecipes.createFoodRecipes(false)
+    }
 }
